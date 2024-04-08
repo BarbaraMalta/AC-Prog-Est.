d784b6b511220b41ddb32b6889abc1bdef4101c6
BarbaraMalta <161264228+BarbaraMalta@users.noreply.github.com>
2024-04-07 23:12:18 -0300

    Enviando códigos da AC6

---
+++ AC6/exercicios_beecrowd.py
@@ -0,0 +1,23 @@
+print("-" * 20, "Exercicio 1: Hello World", "-" * 20)
+print("Hello World!")
+
+print("-" * 20, "Exercicio 2: Extremamente Basico", "-" * 20)
+a = int(input("Informe o valor a: "))
+b = int(input("Informe o valor de b: "))
+x = a + b
+print("X =", x)
+
+print("-" * 20, "Exercicio 3: Calculo Simples", "-" * 20)
+codigo_peca1 = int(input("Informe o codigo da peca 1: ")) 
+num_peca1 = int(input("Informe o numero da peca 1: ")) 
+valor_unitario1 = round(float, 2 (input("Informe o valor unitario da peca 1: ")))
+print(" " * 20)
+codigo_peca2 = int(input("Informe o codigo da peca 2: "))
+num_peca2 = int(input("Informe o numero da peca 2: "))
+valor_unitario2 = round(float(input("Informe o valor unitario da peca 2: ", 2)))
+valor = round(num_peca1 * valor_unitario1 + num_peca2 * valor_unitario2 , 2)
+print("VALOR A PAGAR: R$", valor)
+
+print("-" * 20, "Exercicio 4: O Maior", "-" * 20)
+
+print("-" * 20, "Exercicio 5: Distancia entre dois pontos", "-" * 20)
